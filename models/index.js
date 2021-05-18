b111909ea7e7d6769fe0cd79cac08602e45f424c
Peter Wang <65141087+peterw712@users.noreply.github.com>
2021-05-18 13:52:48 -0400

    foreign key relations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,53 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// Category can have multiple products 
+// Product can only belong to one category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+// Using the ProductTag through model, allows products to have multiple tags 
+// and tags to have many products.
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreignKey: 'tag_id'
+})
+
+//Setting up foreign key relationships that match the column created in the respective models.
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+})
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+})
+
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id'
+})
+
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id'
+})
+
+
 
 module.exports = {
   Product,
